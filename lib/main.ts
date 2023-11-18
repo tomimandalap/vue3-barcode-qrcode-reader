e9c099eb856ef3a05838d50ed4242440cf4f5956
tomimandalap <tommymandalaputra@gmail.com>
2023-11-18 09:48:01 +0700

    rename IconCameraVue to IconCamera

--- lib/main.ts
+++ lib/main.ts
@@ -1,10 +1,10 @@
 import StreamQrcodeBarcodeReader from "./StreamBarcodeReader.vue"
-import IconCameraVue from "./IconCamera.vue"
+import IconCamera from "./IconCamera.vue"
 import { Result } from "@zxing/library"
 
 
 export {
   StreamQrcodeBarcodeReader,
-  IconCameraVue,
+  IconCamera,
   Result,
 }
