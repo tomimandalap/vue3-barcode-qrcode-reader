2ea3808ff73b66af794e38c34d9b8ee6d68950cf
tomimandalap <tommymandalaputra@gmail.com>
2023-11-18 09:00:35 +0700

    takeout define lib

---
+++ lib/main.ts
@@ -0,0 +1,10 @@
+import StreamQrcodeBarcodeReader from "./StreamBarcodeReader.vue"
+import IconCameraVue from "./IconCamera.vue"
+import { Result } from "@zxing/library"
+
+
+export {
+  StreamQrcodeBarcodeReader,
+  IconCameraVue,
+  Result,
+}
