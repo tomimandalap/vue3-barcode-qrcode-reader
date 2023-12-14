5161378c09f6ed8f02dc42b2fa25577482e6d9eb
tomimandalap <tommymandalaputra@gmail.com>
2023-12-14 16:34:08 +0700

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, RouterLink } from 'vue-router'
 </script>
 
 <template>
+  <nav class="bg-gray-900 h-10 flex justify-around items-center text-sm sm:text-base">
+    <RouterLink to="/" class="hover:text-gray-300 text-white">Home</RouterLink>
+    <RouterLink to="/example" class="hover:text-gray-300 text-white">Another example</RouterLink>
+  </nav>
   <RouterView />
 </template>
